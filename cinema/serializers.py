1a1e54f6f8d4a3fded52b7cf5296f44ecd4f0b2c
yevpy <eugene2piskun@gmail.com>
2025-04-24 00:29:35 +0300

    add .ValidationError

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -105,7 +105,10 @@ class TicketSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         data = super(TicketSerializer, self).validate(attrs=attrs)
         Ticket.validate_ticket(
-            attrs["row"], attrs["seat"], attrs["movie_session"]
+            attrs["row"],
+            attrs["seat"],
+            attrs["movie_session"],
+            error_to_raise=serializers.ValidationError
         )
         return data
 
